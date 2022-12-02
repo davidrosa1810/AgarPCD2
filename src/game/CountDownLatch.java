9d7ecea1db21229ffea2a3d4846690e4ee9c6550
RafaelSantiago22 <115997546+RafaelSantiago22@users.noreply.github.com>
2022-12-02 20:19:31 +0000

    tipo especial
    This reverts commit e44e5889ed4450c80c95de18ffd566357ae3cb3e.

---
+++ src/game/CountDownLatch.java
@@ -0,0 +1,19 @@
+package game;
+
+public class CountDownLatch {
+    public CountDownLatch (int count) {
+	this.count = count ;
+    }
+    private int count ;
+    public synchronized void await ()
+	    throws InterruptedException {
+	while(count>0)
+	    wait();
+    }
+    public synchronized void countDown (){
+	count --;
+	if(count==0)
+	    notifyAll();
+    }
+}
+
